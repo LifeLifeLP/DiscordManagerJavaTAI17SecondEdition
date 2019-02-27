3cf7c3cc803164fcb60df09f8115429c3becda45
K.Schulz <K.Schulz@PC-R101-01.mis.local>
2019-02-27 15:11:11 +0100

    added first step for a headless mode

--- src/lifelifelp/io/IO.java
+++ src/lifelifelp/io/IO.java
@@ -16,24 +16,36 @@ import java.io.IOException;
 public class IO {
 	//Ungenutzt soll sp�ter genutzt werden um einen Headless-Betrieb auf einem Server zu erm�glichen
 	@SuppressWarnings("unused")
-	public static boolean loadSetting(){
-		boolean ret = false;
+	public static Settings loadSetting(){
+		Settings settings = new Settings();
 		try {
 			File f = new File("src\\settings.txt");
 			BufferedReader br = new BufferedReader(new FileReader(f));
 			String botID = br.readLine();
+			String headlessMode = br.readLine();
+			if(headlessMode.equals("TRUE")) {
+				settings.setHeadlessmode(true);
+			}else {
+				settings.setHeadlessmode(false);
+			}
 			String botNickname = br.readLine();
 			String canBanBots = br.readLine();
-			String headlessMode = br.readLine();
 			br.close();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
-			return ret;
+			return settings;
 		} catch (IOException e) {
 			e.printStackTrace();
-			return ret;
+			return settings;
 		}
-		return ret;
+		return settings;
+	}
+
+	public static Settings set() {
+		// TODO Auto-generated method stub
+		Settings s = new Settings();
+		
+		return s;
 	}
 
 }
