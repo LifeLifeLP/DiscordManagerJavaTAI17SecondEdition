1aca9e65424fcb82d7f16a28ca04590241794e9b
K.Schulz <K.Schulz@PC-R101-01.mis.local>
2019-02-28 11:30:04 +0100

    added the new Classes to the Listener Register

--- src/lunokaru/login/ThreadDiscordLogin.java
+++ src/lunokaru/login/ThreadDiscordLogin.java
@@ -3,6 +3,8 @@ package lunokaru.login;
 import org.slf4j.LoggerFactory;
 
 import lifelifelp.botfuctions.CommonEvents;
+import lifelifelp.botfuctions.DMEvents;
+import lifelifelp.botfuctions.ServerEvents;
 import sx.blah.discord.api.IDiscordClient;
 
 public class ThreadDiscordLogin extends Thread {
@@ -13,6 +15,8 @@ public class ThreadDiscordLogin extends Thread {
 		logger = LoggerFactory.getLogger(LoginMain.class);
 		discordClient = LoginData.getBotID();
 		discordClient.getDispatcher().registerListener(new CommonEvents());
+		discordClient.getDispatcher().registerListener(new DMEvents());
+		discordClient.getDispatcher().registerListener(new ServerEvents());
 		discordClient.login();
 		logger.info(discordClient.getApplicationName() + " is back. Let's get going!");
 		lunokaru.ui.GuiMain.main(null);
