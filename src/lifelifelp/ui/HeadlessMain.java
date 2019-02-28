6860c10fcf636d4d3f19f00093cb4539d94ea739
K.Schulz <K.Schulz@PC-R101-01.mis.local>
2019-02-28 14:32:37 +0100

    idk

--- src/lifelifelp/ui/HeadlessMain.java
+++ src/lifelifelp/ui/HeadlessMain.java
@@ -3,17 +3,26 @@
  */
 package lifelifelp.ui;
 
+import java.util.ArrayList;
+
+import lifelifelp.io.IOfunctions;
+import lifelifelp.io.SettingsUser;
+import sx.blah.discord.api.IDiscordClient;
+
 /**
  * @author K.Schulz
  *
  */
 public class HeadlessMain {
-
+	static ArrayList<SettingsUser> aSU = new ArrayList<SettingsUser>();
 	/**
 	 * 
 	 */
 	public HeadlessMain() {
-		// TODO Auto-generated constructor stub
+	}
+	public static void main(IDiscordClient iDiscordClient) {
+		aSU = IOfunctions.loadUserDatabase(iDiscordClient);
+		
 	}
 
 }
